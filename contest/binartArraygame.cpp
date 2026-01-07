ecf3d4abd4f641282ce36137adf3125457fe8b58
Justshantam <writetoshantam@gmail.com>
2026-01-07 20:49:06 +0530

    codeforces 1st contest

---
+++ contest/binartArraygame.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        bool hasZero = false;
+        for(int i = 0; i < n; i++){
+            int x;
+            cin >> x;
+            if(x==0)hasZero++;
+        }
+        if(hasZero) cout << "Alice\n";
+        else cout << "Bob\n";
+    }
+    return 0;
+}
