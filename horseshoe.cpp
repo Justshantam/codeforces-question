abe733775afa274db8524e923001523ebb5b9ca1
shantam-sharma <writetoshantam@gmail.com>
2026-01-16 18:29:10 +0530

    using set

--- horseshoe.cpp
+++ horseshoe.cpp
@@ -1,17 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+/*
+int a[4], count = 0;
+for(int i = 0; i<4; i++){
+    cin >> a[i];
+}
+sort(a,a+4);
+for(int i=1; i<4; i++){
+    if(a[i-1] == a[i]){
+        count++;
+    }
+}
+cout<<count;
+return 0;
+*/
+//property of set it it does not consider repeated nums.
 int main() {
-    int a[4], count = 0;
-    for(int i = 0; i<4; i++){
-        cin >> a[i];
+    set<int> s;
+    for(int i = 0; i < 4; i++){
+        int n;
+        cin >> n;
+        s.insert(n);
     }
-    sort(a,a+4);
-    for(int i=1; i<4; i++){
-        if(a[i-1] == a[i]){
-            count++;
-        }
-    }
-    cout<<count;
-    return 0;
+    cout << (4-s.size());
 }
