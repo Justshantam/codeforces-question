0057520ae0c33aa17765dea542a3f1895f9c5088
shantam-sharma <writetoshantam@gmail.com>
2026-01-28 22:24:42 +0530

    modifications

--- findNum.cpp
+++ findNum.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
+/*
 
 int main() {
     vector<long long> v(4);
@@ -14,12 +15,33 @@ int main() {
     cout << abc[0] << " " << abc[1] << " " << abc[2];
     return 0;
 }
-/*
 
 a+b = z-c --- z-a-b = c
 a+c = z-b --- z-a-c = b
 b+c = z-a --- z-b-c = a
 a+b+c = z
 
+
+int main(){
+    long long a,b,c,d;
+    cin >> a >> b >> c >> d;
+    long long sum = (a+b+c+d)/3;
+    for(auto i : {a,b,c,d}){
+        if(i<sum){
+            cout << sum - i << ' ';
+        }
+    }
+}
+
 */
 
+int main(){
+    long long a,b,c,d;
+    cin >> a >> b >> c >> d;
+    long long sum = max({a,b,c,d});
+    for(auto i : {a,b,c,d}){
+        if(i<sum){
+            cout << sum - i << ' ';
+        }
+    }
+}
