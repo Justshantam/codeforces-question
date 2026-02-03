8405901ca0a29db317a0e988deb676adfdf36976
shantam-sharma <writetoshantam@gmail.com>
2026-02-03 20:38:27 +0530

    marathon

---
+++ problems/marathon.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t; cin >> t;
+    while(t--){
+        int a,b,c,d,cnt = 0;
+        cin >> a >> b >> c >> d;
+        if(b > a){cnt += 1;}
+        if(c > a){cnt += 1;}
+        if(d > a){cnt += 1;}
+        cout << cnt << endl;
+    }
+    return 0;
+}
