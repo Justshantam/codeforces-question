1645533d7d05969a7985b80e6bf2386b2de91f00
shantam-sharma <writetoshantam@gmail.com>
2026-02-04 08:53:21 +0530

    hate 3

---
+++ problems/dislike3.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t, k; cin >> t;
+    while(t--){
+        cin >> k;
+        for(int i=1; i<=k; i++){
+            if(i % 3 == 0 || i % 10 == 3) k++;
+        }
+        cout << k << endl;
+    }
+    return 0;
+}
